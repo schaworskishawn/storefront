ccc41ccf3c13b9fe9f874ac1750a9e8e7e5c1efa
Shawn Schaworski <38747567+schaworskishawn@users.noreply.github.com>
2026-07-12 17:25:55 +0000

    Fix plasmic-init import path in plasmic-host route

--- src/app/plasmic-host/page.tsx
+++ src/app/plasmic-host/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
-import "../../plasmic-init";
+import "../../../plasmic-init";
 
 export default function PlasmicHost() {
 	return <PlasmicCanvasHost />;
